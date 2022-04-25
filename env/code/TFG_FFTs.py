77c1c6cf4d31fd7167f1bdd2d50dffa1c1dc1157
ricard <ricard@LAPTOP-SNGFL1VL.localdomain>
2022-04-25 17:17:07 +0200

    TFG_FFT.py new version

--- env/code/TFG_FFTs.py
+++ env/code/TFG_FFTs.py
@@ -12,27 +12,33 @@ IR_input = es.MonoLoader(
        sampleRate=sr)()
 IR_output = es.MonoLoader(
        filename='./AUDIOS_TFG/IRs_separadas/Preguntas_2,3i4/'
-                'IR_128F_6R_sweepstat.wav',
+                'IR_0R_16kF_sweepstat.wav',
+       sampleRate=sr)()
+
+IR_ref = es.MonoLoader(
+       filename='./AUDIOS_TFG/IRs_separadas/Preguntas_2,3i4/'
+                'IR_Bypass_sweepstat.wav',
        sampleRate=sr)()
 
 # Ploteamos las IR
 '''
-fig, ax = plt.subplots(nrows=2, ncols=1)
+fig, ax = plt.subplots(nrows=3, ncols=1)
 ax[0].plot(IR_input, color='b')
 ax[0].set_title("Input IR")
 ax[1].plot(IR_output, color='r')
 ax[1].set_title("Output IR")
+ax[2].plot(IR_ref, color='k')
+ax[2].set_title("Reference IR")
 plt.tight_layout()
-print(len(IR_input))
-print(len(IR_output))
 plt.show()
 exit()
 '''
 # Cogemos la posicion del max de las IR (cogiendo solo la parte positiva)
 pos_max_in = np.argmax(abs(IR_input))
 pos_max_out = np.argmax(abs(IR_output))
+pos_max_ref = np.argmax(abs(IR_ref))
 
-# Recortamos el senyal de manera que el maximo esté en la misma posicion
+# Recortamos los 3 señales de manera que los picos esten alineados
 if pos_max_out>pos_max_in:
     dif = pos_max_out - pos_max_in
     IR_output = IR_output[dif:len(IR_output)]
@@ -40,53 +46,72 @@ else:
     dif = pos_max_in - pos_max_out
     IR_input = IR_input[dif:len(IR_input)]
 
+if pos_max_ref>pos_max_out:
+    dif = pos_max_ref - pos_max_out
+    IR_ref = IR_ref[dif:len(IR_ref)]
+else:
+    dif = pos_max_in - pos_max_ref
+    IR_input = IR_input[dif:len(IR_input)]
+
 # Para que input i output tengan el mismo tamaño y sean pares (para la FFT)
-if len(IR_input)>len(IR_output):
+if (len(IR_input)>len(IR_ref))&(len(IR_ref)>len(IR_output)):
     s = len(IR_output)
-else:
+elif (len(IR_ref)>len(IR_output))&(len(IR_output)>len(IR_input)):
     s = len(IR_input)
-
-if s%2 != 0:
-    s = s-1
-else:
-    s=s
-
-IR_input=IR_input[0:s]
-IR_output=IR_output[0:s]
-
+else: 
+    s = len(IR_ref)
+'''
 print('pos_max_in', np.argmax(abs(IR_input)))
 print('pos_max_out', np.argmax(abs(IR_output)))
+print('pos_max_ref', np.argmax(abs(IR_ref)))
 print('len_in', len(IR_input))
 print('len_out', len(IR_output))
+print('len_ref', len(IR_ref))
+print('s', s)
+exit()
+'''
+if s%2 != 0:
+    s = s-1
+
+IR_input = IR_input[0:s]
+IR_output = IR_output[0:s]
+IR_ref = IR_ref[0:s]
 
 IR_input[IR_input == 0] = np.finfo(float).eps # 0 = num molt petit
 IR_output[IR_output == 0] = np.finfo(float).eps
+IR_ref[IR_ref == 0] = np.finfo(float).eps
 
-spec = es.FFT(size=len(IR_input))
+spec = es.FFT(size=s)
 c2p = es.CartesianToPolar()
-
 IR_input_fft = spec(IR_input)
 IR_output_fft = spec(IR_output)
-trans_func = IR_output_fft/IR_input_fft
-TF_mag, TF_ang = c2p(trans_func)
+IR_ref_fft = spec(IR_ref)
 
-N = len(TF_mag)
+trans_func_out = IR_output_fft/IR_input_fft
+TF_mag_out, TF_ang_out = c2p(trans_func_out)
+trans_func_ref = IR_ref_fft/IR_input_fft
+TF_mag_ref, TF_ang_ref = c2p(trans_func_ref)
+
+N = len(IR_input_fft)
 n = np.arange(N)
 T = N/sr
 freq = n/T
 
-TF_mag = 20*np.log10(TF_mag) # Magnitut en dBs
-TF_min = np.min(TF_mag)
-TF_max = np.max(TF_mag)
+TF_mag_out = 20*np.log10(TF_mag_out) # Magnitut en dBs
+TF_mag_ref = 20*np.log10(TF_mag_ref)
+TF_min = np.min(TF_mag_out)
+TF_max = np.max(TF_mag_out)
 
-plt.subplot(2,1,1)
-plt.semilogx(freq, TF_mag)
+#plt.subplot(2,1,1)
+plt.semilogx(freq, TF_mag_out, color='r')
+plt.semilogx(freq, TF_mag_ref, color='b')
 plt.xlabel('Freq (Hz)')
 plt.ylabel('Amplitude (dB)')
 plt.xlim(10, 32000)
-plt.ylim(TF_min,TF_max+30)
+plt.ylim(TF_min,TF_max)
 plt.title('Magnitud_TF')
-
+plt.show()
+'''
 plt.subplot(2,1,2)
 plt.semilogx(freq, TF_ang)
 plt.xlabel('Freq (Hz)')
@@ -102,7 +127,7 @@ plt.semilogx(freq, 20*np.log10(np.abs(IR_input_fft)))
 plt.xlabel('Freq (Hz)')
 plt.ylabel('Amplitude (dB)')
 plt.xlim(10, 32000)
-plt.ylim(-50,30)
+plt.ylim(-50,50)
 plt.title('IR_input_fft')
 
 plt.subplot(2,1,2)
@@ -110,10 +135,10 @@ plt.semilogx(freq,20*np.log10(np.abs(IR_output_fft)))
 plt.xlabel('Freq (Hz)')
 plt.ylabel('Amplitude')
 plt.xlim(10, 32000)
-plt.ylim(-50, 30)
+plt.ylim(-50, 50)
 plt.title('IR_output_fft')
 plt.tight_layout()
 plt.show()
-
+'''
 
 
