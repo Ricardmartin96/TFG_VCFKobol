e6399f6796390b87372b65796831bc3b44e3eef9
Ricardmartin96 <83163038+Ricardmartin96@users.noreply.github.com>
2022-05-13 18:59:41 +0200

    Add files via upload

---
+++ src/TEST.py
@@ -0,0 +1,29 @@
+import essentia.standard as es
+import matplotlib.pyplot as plt
+import numpy as np
+from Transfer_Function import transfer_function
+from Flat_Region import flat_reg
+
+sr = 48000
+loader = es.MonoLoader(audioStream=0, downmix="mix", filename='./AUDIOS_TFG' 
+                '/IRs_separadas/Preguntas_2,3i4/IR_0R_128F_sweepstat.wav', sampleRate=48000)
+IR_output = loader()
+
+loader = es.MonoLoader(audioStream=0, downmix="mix", filename='./AUDIOS_TFG' 
+                '/IRs_separadas/Preguntas_2,3i4/IR_Loopback_sweepstat.wav', sampleRate=48000)
+IR_input = loader()
+
+
+TF_mag, TF_ang = transfer_function (IR_input, IR_output, sr)
+flat_dB = flat_reg(TF_mag, sr)
+
+
+print(flat_dB)
+
+N = len(TF_mag)
+n = np.arange(N)
+T = N / sr
+freq = n / T
+plt.semilogx(freq, TF_mag)
+plt.xlim(40, 32000)
+plt.show()
---
+++ src/Transfer_Function.py
@@ -0,0 +1,47 @@
+import essentia.standard as es
+import numpy as np
+
+def transfer_function (IR_input, IR_output, sr):
+    c2p = es.CartesianToPolar()
+
+    # Alineamos las IRs
+    pos_max_in = np.argmax(abs(IR_input))
+    pos_max_out = np.argmax(abs(IR_output))
+
+    if pos_max_out > pos_max_in:
+        dif1 = pos_max_out - pos_max_in
+        IR_output = IR_output[dif1:len(IR_output)]
+    else:
+        dif1 = pos_max_in - pos_max_out
+        IR_input = IR_input[dif1:len(IR_input)]
+
+    # Nos aseguramos que tengan la misma longitud y que esta sea par
+    if (len(IR_input) < len(IR_output)):
+        s = len(IR_input)
+    else:
+        s = len(IR_output)
+
+    if s % 2 != 0:
+        s = s - 1
+
+    IR_input = IR_input[0:s]
+    IR_output = IR_output[0:s]
+
+    # Si es 0, lo ponemos a un valor muy proximo a 0
+    IR_input[IR_input == 0] = np.finfo(float).eps
+    IR_output[IR_output == 0] = np.finfo(float).eps
+
+    # Calculamos fft y funcion de transferencia(TF) del output
+    spec = es.FFT(size=s)
+
+    IR_input_fft = spec(IR_input)
+    IR_output_fft = spec(IR_output)
+
+    trans_func = IR_output_fft / IR_input_fft
+    TF_mag, TF_ang = c2p(trans_func)
+    TF_mag = 20*np.log10(TF_mag)
+
+    return TF_mag, TF_ang
+
+
+
